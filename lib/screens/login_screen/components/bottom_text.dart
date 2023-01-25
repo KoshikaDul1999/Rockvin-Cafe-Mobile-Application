69fc7c74dcffb9936d36aa64c20f65060951bbd4
Zaliye Prasandike <73892701+Saliya99@users.noreply.github.com>
2023-01-25 20:21:01 +0530

    Update bottom_text.dart

--- lib/screens/login_screen/components/bottom_text.dart
+++ lib/screens/login_screen/components/bottom_text.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:login_screen/screens/login_screen/animations/change_screen_animation.dart';
-import 'package:login_screen/utils/helper_functions.dart';
+import 'package:RockVin/screens/login_screen/animations/change_screen_animation.dart';
+import 'package:RockVin/utils/helper_functions.dart';
 
 import '../../../utils/constants.dart';
 import 'login_content.dart';
