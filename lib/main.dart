564101d7d377ebda0a0e37081bfb1c35ce2a4f15
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2023-03-09 11:50:55 +0530

    Test commit

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'RockVin',
+      title: 'RockV',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         scaffoldBackgroundColor: kBackgroundColor,
