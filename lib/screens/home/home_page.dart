068363aef54fc69ac8ef822069648512895fe997
Zaliye Prasandike <73892701+Saliya99@users.noreply.github.com>
2023-03-13 12:58:40 +0530

    Create home_page.dart

---
+++ lib/screens/home/home_page.dart
@@ -0,0 +1,43 @@
+import 'package:rock_vin/auth/auth.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  _signout() {
+    //connect Class Auth from app/servises/auth.dart using provider create in main.dart
+    final auth = Provider.of<AuthBase>(context, listen: false);
+    auth.signOut(); //call for signout
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SizedBox(
+          height: 50.0,
+          width: 320.0,
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue,
+                shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(
+                  Radius.circular(50.0),
+                ))),
+            onPressed: _signout,
+            child: const Text(
+              'Signout',
+              style: TextStyle(fontSize: 20, color: Colors.white),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
