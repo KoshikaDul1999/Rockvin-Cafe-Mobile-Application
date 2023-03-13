d8635f18591842bd693bb4d462f194901e7969f4
Zaliye Prasandike <73892701+Saliya99@users.noreply.github.com>
2023-03-13 12:59:45 +0530

    Create firebase_auth.dart

---
+++ lib/auth/auth.dart
@@ -0,0 +1,58 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+abstract class AuthBase {
+  Stream<User?> authStateChanges();
+  Future<void> signOut();
+  Future<void> deleteacc();
+  Future<User?> signInAnonymously();
+  Future<User?> signInWithEmailAndPassword(String email, String password);
+  Future<User?> createUserWithEmailandPassword(String email, String password);
+}
+
+class Auth implements AuthBase {
+  final _firebaseAuth = FirebaseAuth.instance;
+
+  @override
+  //Stream Builder
+  Stream<User?> authStateChanges() => _firebaseAuth.authStateChanges();
+
+  User? get currentUser => _firebaseAuth.currentUser;
+
+  @override
+  Future<User?> signInAnonymously() async {
+    final userCredential = await _firebaseAuth.signInAnonymously();
+    // print("Logged As user ${userCredential.user.uid}");
+    return userCredential.user;
+  }
+
+  @override
+  // SignIn using Email
+  Future<User?> signInWithEmailAndPassword(
+      String email, String password) async {
+    final userCredential =
+        await _firebaseAuth.signInWithCredential(EmailAuthProvider.credential(
+      email: email,
+      password: password,
+    ));
+    return userCredential.user;
+  }
+
+  @override
+  Future<User?> createUserWithEmailandPassword(
+      String email, String password) async {
+    final userCredential = await _firebaseAuth.createUserWithEmailAndPassword(
+        email: email, password: password);
+    return userCredential.user;
+  }
+
+  @override
+  Future<void> signOut() async {
+    await _firebaseAuth.signOut();
+    _firebaseAuth.authStateChanges();
+  }
+
+  @override
+  Future<void> deleteacc() async {
+    await currentUser!.delete();
+  }
+}
